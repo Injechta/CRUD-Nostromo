0ac7b7260c41ffef969b0b603ae552e02e3f24e4
Gregory ELBAJOURY <34866703+Injechta@users.noreply.github.com>
2019-11-25 00:00:12 +0100

    Update sql.sql
    Just before watchin ALien1, job is done

--- Nostromo Crew/sql.sql
+++ Nostromo Crew/sql.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
+-- version 4.6.6deb5
 -- https://www.phpmyadmin.net/
 --
--- Client :  127.0.0.1
--- Généré le :  Ven 25 Mai 2018 à 13:57
--- Version du serveur :  10.1.21-MariaDB
--- Version de PHP :  5.6.30
+-- Client :  localhost
+-- Généré le :  Dim 24 Novembre 2019 à 23:53
+-- Version du serveur :  5.7.28-0ubuntu0.18.04.4
+-- Version de PHP :  7.2.24-1+ubuntu18.04.1+deb.sury.org+1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -28,11 +28,25 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `utilisateurs` (
   `id` int(11) NOT NULL,
-  `nom` varchar(128) NOT NULL,
-  `prenom` varchar(128) NOT NULL,
+  `nom` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
+  `prenom` varchar(128) COLLATE utf8_unicode_ci NOT NULL,
   `age` int(11) NOT NULL,
-  `adresse` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `adresse` varchar(128) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `utilisateurs`
+--
+
+INSERT INTO `utilisateurs` (`id`, `nom`, `prenom`, `age`, `adresse`) VALUES
+(1, 'Ripley', 'Ellen Louise', 30, 'Chambre de biostase 17, Nostromo'),
+(2, 'Dallas', 'Arthur Koblenz', 47, 'Chambre de biostase 1, Nostromo'),
+(3, 'Kane', 'Thomas', 39, 'Chambre de biostase 8, Nostromo'),
+(6, '', 'Ash', 0, 'Muthur, Nostromo'),
+(8, 'Lambert', 'Joan Marie', 29, 'Chambre de biostase 6, Nostromo'),
+(9, 'Parker', 'Dennis', 42, 'Chambre de biostase 9, Nostromo'),
+(10, 'Brett', 'Samuel', 53, 'Chambre de biostase 12, Nostromo'),
+(11, '', 'Jones', 2, 'Nostromo');
 
 --
 -- Index pour les tables exportées
@@ -52,7 +66,7 @@ ALTER TABLE `utilisateurs`
 -- AUTO_INCREMENT pour la table `utilisateurs`
 --
 ALTER TABLE `utilisateurs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
